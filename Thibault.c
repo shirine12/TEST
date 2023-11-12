f875092b5fcc0739210814a24a9413835f49a55c
AlbanePATUREL <150240551+AlbanePATUREL@users.noreply.github.com>
2023-11-12 15:02:15 +0100

    Update Thibault.c

--- Thibault.c
+++ Thibault.c
@@ -4,11 +4,16 @@
 #include <windows.h>
 #include <unistd.h>
 
-void DeplacementBalle(int* balleX,int* balleY,int*vx, int*vy){
+void DeplacementBalle(int* balleX,int* balleY,int*vx, int*vy, unsigned char tab[Nc][Nl]){
+    tab[*balleX][*balleY]=' ';
+    if ((tab[*balleX+*vx][*balleY+*vy]=='0') || (tab[*balleX+*vx][*balleY+*vy]=='X')){
+        *vy = *vy*(-1);
+        *vx = -1 * *vx;
+    }
         *balleX = *balleX + *vx;
         *balleY = *balleY + *vy;
 
-        if (*balleY <= 0 || *balleY >= 9) {//(hauteur matrice-1)
+        if (*balleY <= 0 || *balleY >= 19) {//(hauteur matrice-1)
             *vy = *vy*(-1); // Inverse la direction verticale
         }
         if (*balleX <= 0 || *balleX >= 9) {//(largeur matrice-1)
