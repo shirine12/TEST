1ea757bf9cb4935c7603d08b8db54481d5bb62eb
Thibault-prog <149437436+Thibault-prog@users.noreply.github.com>
2023-11-12 22:17:56 +0100

    Update Thibault.c

--- Thibault.c
+++ Thibault.c
@@ -11,17 +11,17 @@ void DeplacementBalle(int* balleX,int* balleY,int*vx, int*vy, unsigned char tab[
         *vx = -1 * *vx;
         system("cls");
     }
-        *balleX = *balleX + *vx;
-        *balleY = *balleY + *vy;
+    *balleX = *balleX + *vx;
+    *balleY = *balleY + *vy;
 
-        if (*balleY <= 0 || *balleY >= 19) {//(hauteur matrice-1)
-            *vy = *vy*(-1); // Inverse la direction verticale
+    if (*balleY <= 0 || *balleY >= 19) {//(hauteur matrice-1)
+        *vy = *vy*(-1); // Inverse la direction verticale
         system("cls");
-        }
-        if (*balleX <= 0 || *balleX >= 9) {//(largeur matrice-1)
-            *vx = -1 * *vx; // Inverse la direction horizontale
-         system("cls");
-        }
+    }
+    if (*balleX <= 0 || *balleX >= 9) {//(largeur matrice-1)
+        *vx = -1 * *vx; // Inverse la direction horizontale
+        system("cls");
+    }
 }
 
 void gotoligcol( int lig, int col )
@@ -32,6 +32,8 @@ void gotoligcol( int lig, int col )
     mycoord.Y = lig;
     SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), mycoord );
 }
+
+
 void ShowConsoleCursor(int showFlag){
     //Permet de retirer le curseur (trouvé à partir de recherches internet qui ont demandés de la réflexion)//
 HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
