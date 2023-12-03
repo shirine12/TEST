c7b8f9260bd95287c8d1a9ecf6292e2aaf22cda0
Thibault-prog <149437436+Thibault-prog@users.noreply.github.com>
2023-12-03 02:41:57 +0100

    Update Albane.c

--- Albane.c
+++ Albane.c
@@ -1,4 +1,3 @@
-
 #ifdef _WIN32
 #include "LIBRAIRIE SS PROG.h"
 #include <windows.h>
@@ -11,6 +10,11 @@
 #define MAGENTA_COLOR "\x1b[35m"
 #define CYAN_COLOR "\x1b[36m"
 #define RESET_COLOR "\x1b[0m"
+#define GRAS "\033[0m"
+#define RESET_GRAS "\033[0m"
+#define FOND_BLANC "\033[0;47m"
+#define RESET_FOND "\033[0m"
+#define NOIR_COLOR "\033[0;30m"
 
 void menu(){
     printf("-------------------------------------------\n"
@@ -22,21 +26,22 @@ void menu(){
            "               S N O O P Y\n"
            "\n"
            "--------------             ----------------\n"
-           "                                       \n"
-           "                  ........           \n"
-           "                 .      .....          \n"
-           "                 . .   ......       \n"
-           "             ....       .....        \n"
-           "             .     ...-  ....          \n"
-           "               ...            \n\n"
+           "                                           \n"
+           "                  ........                 \n"
+           "                 .      .....              \n"
+           "                 . .   ......              \n"
+           "             ....       .....              \n"
+           "             .     ...-  ....              \n"
+           "               ...                       \n\n"
            "-------------------------------------------\n"
-           "\n"
+                     "\n"
            "1. Afficher les regles\n"
            "2. Lancer une partie\n"
            "3. Charger une partie\n"
            "4. Afficher les mots de passe\n"
            "5. Afficher les scores\n"
-           "6. Quitter\n\n"
+           "6. Reprendre depuis derniere sauvegarde\n"
+           "7. Quitter\n\n"
            "-------------------------------------------\n");
     sleep(1);
     gotoligcol(0,0);
@@ -57,7 +62,7 @@ void menu(){
            "             .     ..._  ....       \n"
            "               ...     ..            \n\n"
            "-------------------------------------------\n"
-           "\n"
+                  "\n"
            "1. Afficher les regles\n"
            "2. Lancer une partie\n"
            "3. Charger une partie\n"
@@ -72,9 +77,9 @@ void InitialiserVie(int* a) {
 }
 
 void AfficherNiv(int x,int y, int balleX, int balleY, unsigned char tab[Nc][Nl], int block, int NIV) {
+    int i, j;
     if (NIV == 1) {
-        int i, j;
-        gotoligcol(1, 1);
+        gotoligcol(1, 0);
         for (i = 0; i < Nc; i++) {
             for (j = 0; j < Nl; j++) {
                 tab[x][y] = NULL;
@@ -104,69 +109,47 @@ void AfficherNiv(int x,int y, int balleX, int balleY, unsigned char tab[Nc][Nl],
                         printf("%c", 0xFE);
                     }
                 } else
-                    printf(RESET_COLOR"%c", tab[i][j]);
+                    printf(RESET_COLOR"");
+                    printf("%c", tab[i][j]);
             }
             printf("\n");
         }
-        ShowConsoleCursor(FALSE);
-        gotoligcol(11,0);
-        printf(CYAN_COLOR "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF);
-        gotoligcol(0,0);
-        printf(CYAN_COLOR"%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF);
-        gotoligcol(0,21);
-        printf("%c", 0xDB);
-        gotoligcol(1,21);
-        printf("%c", 0xDB);
-        gotoligcol(2,21);
-        printf("%c", 0xDB);
-        gotoligcol(3,21);
-        printf("%c", 0xDB);
-        gotoligcol(4,21);
-        printf("%c", 0xDB);
-        gotoligcol(5,21);
-        printf("%c", 0xDB);
-        gotoligcol(6,21);
-        printf("%c", 0xDB);
-        gotoligcol(7,21);
-        printf("%c", 0xDB);
-        gotoligcol(8,21);
-        printf("%c", 0xDB);
-        gotoligcol(9,21);
-        printf("%c", 0xDB);
-        gotoligcol(10,21);
-        printf("%c", 0xDB);
-        //premier plan
-        gotoligcol(0,0);
-        printf("%c", 0xDB);
-        gotoligcol(1,0);
-        printf("%c", 0xDB);
-        gotoligcol(2,0);
-        printf("%c", 0xDB);
-        gotoligcol(3,0);
-        printf("%c", 0xDB);
-        gotoligcol(4,0);
-        printf("%c", 0xDB);
-        gotoligcol(5,0);
-        printf("%c", 0xDB);
-        gotoligcol(6,0);
-        printf("%c", 0xDB);
-        gotoligcol(7,0);
-        printf("%c", 0xDB);
-        gotoligcol(8,0);
-        printf("%c", 0xDB);
-        gotoligcol(9,0);
-        printf("%c", 0xDB);
-        gotoligcol(10,0);
-        printf("%c", 0xDB);
-        printf(RESET_COLOR"");
-
-
-
-
-
-
     }
     if(NIV==2){
+        gotoligcol(1, 0);
+        for (i = 0; i < Nc; i++) {
+            for (j = 0; j < Nl; j++) {
+                tab[x][y] = NULL;
+                tab[balleX][balleY] = NULL;
+                if (i == x && j == y) {
+                    printf("%c", 0xF4);
+                }
+                /*if((i==0&&j==1)||(i==9&&j==20)||(i==0&&j==20)||(i==9&&j==1)){
+                    printf("%c", 0xA9);
+                }*/
+
+                if (i == balleX && j == balleY) {
+                    printf(MAGENTA_COLOR "%c", 0x9D);
+                }/*
+                if ((i == 8 && j == 5) || (i == 9 && j == 5) || (i == 1 && j == 16)) {
+                    printf("%c", 0xCE); //bloc bouge pas
+                }*/
+                if ((i ==2&& j == 1) || (i == 2 && j == 2) || (i == 2 && j == 3)||(i ==2&& j == 4) || (i == 2 && j == 5) || (i == 2 && j == 6)||(i ==2&& j == 7) || (i == 9 && j == 10) || (i == 8 && j == 11)) {
+                    printf("%c", 0xB2); //Bloc qui tue
+                }
+                if ((i == 5 && j == 3)||(i==5&&j==18)) {
+
+                    if (block == 1) {
+                        printf(" ");
+                    } else {
+                        printf("%c", 0xFE);
+                    }
+                } else
+                    printf(RESET_COLOR"");
+                printf("%c", tab[i][j]);
+            }
+            printf("\n");
+        }
 
     }
     if(NIV==3){
@@ -175,6 +158,58 @@ void AfficherNiv(int x,int y, int balleX, int balleY, unsigned char tab[Nc][Nl],
     if(NIV==4){
 
     }
+    ShowConsoleCursor(FALSE);
+    gotoligcol(11,0);
+    printf(CYAN_COLOR "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF);
+    gotoligcol(0,0);
+    printf(CYAN_COLOR"%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c", 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF, 0xDF);
+    gotoligcol(0,22);
+    printf("%c", 0xDB);
+    gotoligcol(1,22);
+    printf("%c", 0xDB);
+    gotoligcol(2,22);
+    printf("%c", 0xDB);
+    gotoligcol(3,22);
+    printf("%c", 0xDB);
+    gotoligcol(4,22);
+    printf("%c", 0xDB);
+    gotoligcol(5,22);
+    printf("%c", 0xDB);
+    gotoligcol(6,22);
+    printf("%c", 0xDB);
+    gotoligcol(7,22);
+    printf("%c", 0xDB);
+    gotoligcol(8,22);
+    printf("%c", 0xDB);
+    gotoligcol(9,22);
+    printf("%c", 0xDB);
+    gotoligcol(10,22);
+    printf("%c", 0xDB);
+    //premier plan
+
+    /* gotoligcol(0,0);
+     printf("%c", 0xDB);
+     gotoligcol(1,0);
+     printf("%c", 0xDB);
+     gotoligcol(2,0);
+     printf("%c", 0xDB);
+     gotoligcol(3,0);
+     printf("%c", 0xDB);
+     gotoligcol(4,0);
+     printf("%c", 0xDB);
+     gotoligcol(5,0);
+     printf("%c", 0xDB);
+     gotoligcol(6,0);
+     printf("%c", 0xDB);
+     gotoligcol(7,0);
+     printf("%c", 0xDB);
+     gotoligcol(8,0);
+     printf("%c", 0xDB);
+     gotoligcol(9,0);
+     printf("%c", 0xDB);
+     gotoligcol(10,0);
+     printf("%c", 0xDB);*/
+    printf(RESET_COLOR"");
 }
 void JeuGAGNE(int v, int S){
     char touche;
